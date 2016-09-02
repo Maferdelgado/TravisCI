6a9107ecc948291ba597c4f23ee91764bd5e2573
MariferDelgado <fer1995am@gmail.com>
2016-09-01 20:42:51 -0500

    TravisCI....

--- src/traviscimafer/TravisCIMafer.java
+++ src/traviscimafer/TravisCIMafer.java
@@ -10,9 +10,10 @@ public class Menu {
 }
     public static void main(String[] args)
     {
+        System.out.println("Universidad Laica Eloy Alfaro de Manabí");
         System.out.println("Calculadora Básica");
         System.out.println("Ingeniería de Software");
-         System.out.println("Ing. Diego Toala");
+        System.out.println("Ing. Diego Toala");
         
         int op=0;
         double n1,n2,multiplicacion,suma,division,resta;
